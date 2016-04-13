08e54b81443508d07c008589a288f8a5671d3888
Samuel Harrison Wolfe <samuelhwolfe@Samuel-Wolfes-MacBook-Pro.local>
2016-04-13 16:54:28 -0700

    added isNaN to verify input text and created a corresponding alert message

--- week1/js/main.js
+++ week1/js/main.js
@@ -3,45 +3,64 @@ var reset = "";
 
 function addNumber(){
 	
-	//variable that translates user input from text to integers
-	var integers = parseFloat(document.getElementById("userInput").value, 10);
+	var x = document.getElementById("userInput").value;
 	
-	//pushes 'var integers' into 'var numbers'
-	numbers.push(integers);
+	if (isNaN(x)) {
+		//alert("must be a number");
+		document.getElementById("userInput").value='';
+		document.getElementById("error").innerHTML = "Dude, that's not a number";
+	}
 	
-	//resets the input text box
-	document.getElementById("userInput").value=''; 
+	else {
+		
+		//clears the error message when a number is pressed
 
-	//calculates array length and outputs it to #total
-	document.getElementById("total").innerHTML = numbers.length; 
+		document.getElementById("error").innerHTML = '';
 
-	//use reduce to calculate sum of numbers array
-	var sum = numbers.reduce(function(prev,next) {
-		prev += next;
-		return prev;
-	},0);
-	
-	//output 'var sum' to #sum
-	document.getElementById("sum").innerHTML = sum;
-	
-	var max = Math.max.apply(null, numbers);
-	
-	//output 'var max' to #max
-	document.getElementById("max").innerHTML = max;
-	
-	var avg = sum / numbers.length
-	
-	//output 'var avg' to #avg
-	document.getElementById("avg").innerHTML = avg;
-	
-	console.log(numbers);
-	console.log("reduce(): " + sum);
-	console.log(max);
-	console.log(avg);
+		//variable that translates user input from text to integers
+		var integers = parseFloat(document.getElementById("userInput").value, 10);
+
+		//pushes 'var integers' into 'var numbers'
+		numbers.push(integers);
+
+		//resets the input text box
+		document.getElementById("userInput").value=''; 
+
+		//calculates array length and outputs it to #total
+		document.getElementById("total").innerHTML = numbers.length; 
+
+		//use reduce to calculate sum of numbers array
+		var sum = numbers.reduce(function(prev,next) {
+			prev += next;
+			return prev;
+		},0);
+
+		//output 'var sum' to #sum
+		document.getElementById("sum").innerHTML = sum;
+
+		var max = Math.max.apply(null, numbers);
+
+		//output 'var max' to #max
+		document.getElementById("max").innerHTML = max;
+
+		var avg = sum / numbers.length
+
+		//output 'var avg' to #avg
+		document.getElementById("avg").innerHTML = avg;
+
+
+		console.log(numbers);
+		console.log("reduce(): " + sum);
+		console.log(max);
+		console.log(avg);
+		console.log(x);
+
+			}
 }
 
 function resetForm(){
 	
+	document.getElementById("error").innerHTML = '';
 	var element = document.getElementById("total");
 	element.innerHTML = reset;
 	var element = document.getElementById("sum");
@@ -53,8 +72,9 @@ function resetForm(){
 	
 	numbers.length = 0;
 	
-
 }
+
+
 
 
 
