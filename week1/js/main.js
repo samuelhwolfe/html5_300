aac9423c132e533b1999c61d497fed21757d946b
Samuel Harrison Wolfe <samuelhwolfe@Samuel-Wolfes-MacBook-Pro.local>
2016-04-13 11:07:59 -0700

    created the functions for sum, max, and avg

--- week1/js/main.js
+++ week1/js/main.js
@@ -3,16 +3,45 @@ var numbers = [];
 
 function addNumber(){
 
-
-	numbers.push(document.getElementById("userInput").value);
+	//variable that translates user input from text to integers
+	var integers = parseFloat(document.getElementById("userInput").value, 10);
+	
+	//pushes 'var integers' into 'var numbers'
+	numbers.push(integers);
+	
 	//resets the input text box
 	document.getElementById("userInput").value=''; 
-//	//calculates array length and outputs it to #total
-//	document.getElementById("total").innerHTML = numbers.length; 
-
 
 	
+	//calculates array length and outputs it to #total
+	document.getElementById("total").innerHTML = numbers.length; 
+
+	//use reduce to calculate sum of numbers array
+	var sum = numbers.reduce(function(prev,next) {
+		prev += next;
+		return prev;
+	},0);
+	
+	//output 'var sum' to #sum
+	document.getElementById("sum").innerHTML = sum;
+	
+	
+	var max = Math.max.apply(null, numbers);
+	
+	//output 'var max' to #max
+	document.getElementById("max").innerHTML = max;
+	
+	
+	var avg = sum / numbers.length
+	
+	//output 'var avg' to #avg
+	document.getElementById("avg").innerHTML = avg;
+	
 	console.log(numbers);
+	console.log("reduce(): " + sum);
+	//console.log(Math.max.apply(null, numbers));
+	console.log(max);
+	console.log(avg);
 	
 }
 
