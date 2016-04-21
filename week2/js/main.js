2b4e8a94b946334c10f295fcff2ec68c46de9cfe
Samuel Harrison Wolfe <samuelhwolfe@gmail.com>
2016-04-20 20:33:01 -0700

    added js to reveal form when new item button is clicked

--- week2/js/main.js
+++ week2/js/main.js
@@ -1,7 +1,15 @@
-var data = [];
+//show the form when "New item" button is clicked
 
-function submitData(){
-	
-	
-	
+function showForm() {
+	document.getElementById("module-2").style.display = "block";
+	console.log(showForm);
 }
+
+//array that will hold the user generated objects
+
+var myArray = [];
+
+//film object (values to be populated by user input)
+var film = {
+	
+};
